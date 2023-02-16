0dd515cd3e6919fa09ca6bcbf59d04e55da9cf80
AlexBerett <berett.89@gmail.com>
2023-02-16 11:47:39 +0300

    Imported func_deduction

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from funcs import func_sum
+from funcs import func_sum, func_deduction
 
 a = float(input("Введите а = "))
 b = float(input("Введите b = "))
 
 print(func_sum(a,b))
+print(func_deduction(a,b))
