7184c70bd40977c9740b6e00b99f66bde7c8b831
AlexBerett <berett.89@gmail.com>
2023-02-16 11:38:25 +0300

    Added deduction

--- funcs.py
+++ funcs.py
@@ -8,3 +8,12 @@ def func_sum(a, b):
     c = a + b
     return c
 
+def func_deduction(a, b):
+    """
+    Функция возвращает вычитание значений a и b
+    :param a:
+    :param b:
+    :return: c как вычитание a и b
+    """
+    c = a - b
+    return c
